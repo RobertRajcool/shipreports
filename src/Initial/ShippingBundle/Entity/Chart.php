acd1af9700f267571dd42962a6d32f9e28d712dd
lawrance <lawrance@star-systems.in>
2016-02-11 20:05:55 +0530

    Chart Generation

---
+++ src/Initial/ShippingBundle/Entity/Chart.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace Initial\ShippingBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Chart
+ *
+ * @ORM\Table(name="chart")
+ * @ORM\Entity(repositoryClass="Initial\ShippingBundle\Repository\ChartRepository")
+ */
+class Chart
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="kpiname", type="string", length=255)
+     */
+    private $kpiname;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fromdate", type="date")
+     */
+    private $fromdate;
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="todate", type="date")
+     */
+    private $todate;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="kpiname", type="string", length=255)
+     */
+    private $elementDetailsId;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set kpiname
+     *
+     * @param string $kpiname
+     * @return Chart
+     */
+    public function setKpiname($kpiname)
+    {
+        $this->kpiname = $kpiname;
+
+        return $this;
+    }
+
+    /**
+     * Get kpiname
+     *
+     * @return string 
+     */
+    public function getKpiname()
+    {
+        return $this->kpiname;
+    }
+
+    /**
+     * Set fromdate
+     *
+     * @param \DateTime $fromdate
+     * @return Chart
+     */
+    public function setFromdate($fromdate)
+    {
+        $this->fromdate = $fromdate;
+
+        return $this;
+    }
+
+    /**
+     * Get fromdate
+     *
+     * @return \DateTime 
+     */
+    public function getFromdate()
+    {
+        return $this->fromdate;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getTodate()
+    {
+        return $this->todate;
+    }
+
+    /**
+     * @param \DateTime $todate
+     */
+    public function setTodate($todate)
+    {
+        $this->todate = $todate;
+    }
+
+    /**
+     * @return string
+     */
+    public function getElementDetailsId()
+    {
+        return $this->elementDetailsId;
+    }
+
+    /**
+     * @param string $elementDetailsId
+     */
+    public function setElementDetailsId($elementDetailsId)
+    {
+        $this->elementDetailsId = $elementDetailsId;
+    }
+
+
+    function get_months($date1, $date2)
+    {
+        $time1  = strtotime($date1);
+        $time2  = strtotime($date2);
+        $my     = date('mY', $time2);
+
+        $months = array(date('F', $time1));
+
+        while($time1 < $time2) {
+            $time1 = strtotime(date('Y-m-d', $time1).' +1 month');
+            if(date('mY', $time1) != $my && ($time1 < $time2))
+                $months[] = date('F', $time1);
+        }
+
+        $months[] = date('F', $time2);
+        return $months;
+    }
+
+}
---
+++ web/js/504f529_part_1_ajax_jquery_symfony_3.js
@@ -0,0 +1,80 @@
+/**
+ * Created by lawrance on 30/1/16.
+ */
+$(document).ready(function ()
+{
+
+
+
+
+    $('#reading_kpi_values_monthdetail_day').hide();
+    $('#initial_shipping_bundle_add_excel_file_dataofmonth_day').hide();
+    $('#chart_fromdate_day').hide();
+    $('#chart_todate_day').hide();
+
+    $('#shipid').change(function()
+    {
+
+        $('#kpiid option:gt(0)').remove();
+        var data = {shipid : $('#shipid').val()};
+
+        if($(this).val())
+        {
+            $.ajax({
+                type: "post",
+                data: data,
+                url: "/readingkpivalues/kpilist",
+                success: function(data)
+                {
+
+                    $.each(data.kpiNameArray, function(i, listkpi) {
+
+                        $('#kpiid').append($('<option>', {
+                            value: listkpi.id, text : listkpi.kpiName
+                        }));
+                    });
+
+
+                },
+                error: function(XMLHttpRequest, textStatus, errorThrown)
+                {
+                    alert('Error : ' + errorThrown);
+                }
+            });
+        }
+    });
+
+    $('#kpiid').change(function()
+    {
+
+
+        $('#elementId option:gt(0)').remove();
+        var mydata = {elementId : $('#kpiid').val()};
+
+        if($(this).val())
+        {
+            //alert('call function');
+            $.ajax({
+                type: "post",
+                data: mydata,
+                url: "/readingkpivalues/elementlist",
+                success: function(data)
+                {
+                    // alert(data);
+                    $.each(data.ElementNameArray, function(i, listelemnt) {
+
+                        $('#elementId').append($('<option>', {
+                            value: listelemnt.id, text : listelemnt.elementName
+                        }));
+                    });
+
+
+                },
+                error: function(XMLHttpRequest, textStatus, errorThrown)
+                {
+                    alert('Error : ' + errorThrown);
+                }
+            });
+        }
+    });
+});
