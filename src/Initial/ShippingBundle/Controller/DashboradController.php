2137c9f46ecfedf110121157b237c2dbbf8b947d
Hariprakash Sambath <hariprakashsambath@gmail.com>
2016-07-14 15:17:25 +0530

    Corrections in dashboard scorecard year view graph

--- src/Initial/ShippingBundle/Controller/DashboradController.php
+++ src/Initial/ShippingBundle/Controller/DashboradController.php
@@ -625,6 +625,7 @@ class DashboradController extends Controller
                         'rankinKpiCount' => $rKPICount,
                         'currentmonth' => '',
                         'currentyear' => $yChange,
+                        'heading' => 'Management Performance'
                     )
                 );
             } else {
@@ -670,7 +671,8 @@ class DashboradController extends Controller
                 'yearAvgScore' => $yearValue['yearAvgScore'],
                 'yearMonthName' => $yearValue['yearMonthName'],
                 'currentYear' => $yearValue['currentYear'],
-                'kpiNameList' => $yearValue['kpi_list']
+                'kpiNameList' => $yearValue['kpi_list'],
+                'heading' => 'Management Performance'
             ));
             return $response;
         } else {
