a45897246b119c22118ebf63d3de6e7a41231871
Robert <lawrance@star-systems.in>
2016-07-14 18:29:12 +0530

    Changes in Route url

--- src/Initial/ShippingBundle/Controller/PieChartDashboardController.php
+++ src/Initial/ShippingBundle/Controller/PieChartDashboardController.php
@@ -3,11 +3,27 @@
 namespace Initial\ShippingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Initial\ShippingBundle\Form\ScorecardDataImportType;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\Response;
+/**
+ * PieChartDashboardController.
+ *
+ * @Route("/piechart")
+ */
 class PieChartDashboardController extends Controller
 {
-    public function indexAction($name)
+    /**
+     * Lists all PieChartDashboard Elements.
+     *
+     * @Route("/{serialized}/listall", name="vessel_piechart")
+     */
+    public function indexAction($serialized)
     {
-        return $this->render('', array('name' => $name));
+        return $this->render('InitialShippingBundle:DataVerficationRanking:backup.html.twig');
     }
 }
